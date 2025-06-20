9e5df00428baed1f40a70b5b0b9328eb90dcafb0
Elder ds <146546484+elderds16@users.noreply.github.com>
2025-06-20 23:19:44 +0200

    Update Program.cs

--- Album.Api/Program.cs
+++ Album.Api/Program.cs
@@ -18,11 +18,11 @@ public class Program
         InitializeDatabase(app);
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
 
         app.UseSwagger();
         app.UseSwaggerUI();
