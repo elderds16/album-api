32ac0138b345712d04aec3ce238b7ff5fec479da
Elder ds <146546484+elderds16@users.noreply.github.com>
2025-06-20 19:39:21 +0200

    Update GreetingService.cs

--- Album.Api/Services/GreetingService.cs
+++ Album.Api/Services/GreetingService.cs
@@ -7,7 +7,7 @@ public class GreetingService
 {
     public GreetingResponse GetGreeting(string? name)
     {
-        var hostname = Dns.GetHostName();
+        var hostname = Dns.GetHostName(); // Get the hostname of the server (Opdracht 4.1)
         var message = string.IsNullOrWhiteSpace(name) ? $"Hello World from {hostname} v2" : $"Hello {name} from {hostname} v2";
         return new GreetingResponse(message);
     }
